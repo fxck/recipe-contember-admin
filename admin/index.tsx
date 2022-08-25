85c43cfd08d78ad4b9b87b0cf6e548ade8125392
Ales Rechtorik <foxxcz@gmail.com>
2022-08-25 21:26:13 +0200

    headless

--- admin/index.tsx
+++ admin/index.tsx
@@ -1,17 +1,36 @@
 import * as React from 'react'
-import { ApplicationEntrypoint, Pages, runReactApp } from '@contember/admin'
+import { ApplicationEntrypoint, LoginEntrypoint, Pages, runReactApp } from '@contember/admin'
 import { Layout } from './components/Layout'
 import '@contember/admin/style.css'
 
-runReactApp(
-	<ApplicationEntrypoint
-		basePath={import.meta.env.BASE_URL}
-		apiBaseUrl={import.meta.env.VITE_CONTEMBER_ADMIN_API_BASE_URL}
-		sessionToken={import.meta.env.VITE_CONTEMBER_ADMIN_SESSION_TOKEN}
-		project={import.meta.env.VITE_CONTEMBER_ADMIN_PROJECT_NAME}
-		stage="live"
-		defaultDimensions={{ locale: ['en'] }}
-		envVariables={{ WEB_URL: import.meta.env.VITE_CONTEMBER_ADMIN_WEB_URL }}
-		children={<Pages layout={Layout} children={import.meta.globEager('./pages/**/*.tsx')} />}
-	/>,
-)
+const apiBaseUrl = import.meta.env.VITE_CONTEMBER_ADMIN_API_BASE_URL as string
+const loginToken = import.meta.env.VITE_CONTEMBER_ADMIN_LOGIN_TOKEN as string
+const projectSlug = import.meta.env.VITE_CONTEMBER_ADMIN_PROJECT_NAME as string
+
+if (window.location.pathname === '/') {
+    // Login page
+    runReactApp(
+        <LoginEntrypoint
+            apiBaseUrl={apiBaseUrl}
+            loginToken={loginToken}
+            projects={[projectSlug]}
+            formatProjectUrl={it => `/${it.slug}/`}
+        />,
+    )
+} else if (window.location.pathname.startsWith('/' + projectSlug)) {
+    // Administration itself page
+    runReactApp(
+        <ApplicationEntrypoint
+            basePath={`/${projectSlug}/`}
+            apiBaseUrl={apiBaseUrl}
+            project={projectSlug}
+            stage="live"
+						defaultDimensions={{ locale: ['en'] }}
+						envVariables={{ WEB_URL: import.meta.env.VITE_CONTEMBER_ADMIN_WEB_URL }}
+            children={<Pages layout={Layout} children={import.meta.glob('./pages/**/*.tsx')} />}
+        />
+    )
+} else {
+    // Page not found - redirect to login
+    window.location.href = '/'
+}
