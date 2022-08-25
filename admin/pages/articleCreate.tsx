554c3a533b377726489d97ed93a1de6df6b91bd0
Ales Rechtorik <foxxcz@gmail.com>
2022-08-25 20:20:34 +0200

    upload

--- admin/pages/articleCreate.tsx
+++ admin/pages/articleCreate.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react'
-import { CreatePage, RichTextField, TextField } from '@contember/admin'
+import { CreatePage, RichTextField, TextField, AnyFileRepeater } from '@contember/admin'
 
 export default () => (
   <CreatePage
@@ -9,5 +9,6 @@ export default () => (
   >
     <TextField field="title" label="Title" />
     <RichTextField field="content" label="Content" />
+    <AnyFileRepeater field="files" label="Any upload" urlField="file.url" orderBy="order" />
   </CreatePage>
 )
